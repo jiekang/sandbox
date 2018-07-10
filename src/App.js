7ca15fdf0fa6e68c6e99bc47a0503013c6075ac9
Jie Kang <jkang@redhat.com>
2018-07-10 12:44:16 -0400

    Update main page with login placeholder component. Add react-redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { Login } from './login/Login.js';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome.</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Login>
+        </Login>
       </div>
     );
   }
---
+++ src/login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+    render() {
+        return (
+          <div>
+            <p>Login!</p>
+          </div>
+        )
+    }
+
+}
