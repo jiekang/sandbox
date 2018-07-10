003d4517277e5a8538267a2ccad90730e1158c04
Jie Kang <jkang@redhat.com>
2018-07-10 16:10:58 -0400

    Add Login functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 
-import { Login } from './login/Login.js';
+import { Login, AUTH_KEY } from './login/Login.js';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome.</h1>
-        </header>
-        <Login>
-        </Login>
-      </div>
-    );
+    if (this.login()) {
+      return (
+        <div className="UI-app">
+          <header className="UI-app-header">
+            <h1 className="UI-app-title">Welcome.</h1>
+          </header>
+        </div>
+      );
+    } else {
+      return (
+        <Login></Login>
+      )
+    }
+  }
+
+  login() {
+    if (localStorage.getItem(AUTH_KEY)) {
+      return true;
+    }
+    let query = window.location.search.substr(1);
+    let result = {};
+    query.split('&').forEach(function(part) {
+      let item = part.split('=');
+      result[item[0]] = decodeURIComponent(item[1]);
+    });
+
+    if (result['token_json']) {
+      localStorage.setItem(AUTH_KEY, result['token_json']);
+      let here = window.location.href.split('?')[0];
+      window.location.replace(here);
+    }
+
+    return false;
   }
 }
 
--- src/login/Login.js
+++ src/login/Login.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
-export class Login extends Component {
-    render() {
-        return (
-          <div>
-            <p>Login!</p>
-          </div>
-        )
-    }
+import './Login.css';
 
+export const AUTH_KEY = 'auth_token';
+export const AUTH_LOGIN_URL = 'https://auth.openshift.io/api/login';
+
+export class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  render() {
+      return (
+        <div className="UI-login">
+          <button className="UI-login-button" onClick={this.onClick}>Log In</button>
+        </div>
+      )
+  }
+
+  onClick() {
+    let here = window.location.href;
+    window.location.replace(AUTH_LOGIN_URL + '?redirect=' + encodeURIComponent(here));
+  }
 }
