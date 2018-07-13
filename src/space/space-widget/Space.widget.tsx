7d47c272deb0392c74878a2e84ba18c0c099d2b5
Andrew Azores <me@andrewazor.es>
2018-07-13 12:07:29 -0400

    Add SpaceItem functional component

--- src/space/space-widget/Space.widget.tsx
+++ src/space/space-widget/Space.widget.tsx
@@ -13,19 +13,22 @@ export interface SpaceWidgetProps {
   spaces: Space[]
 }
 
+const SpaceItem = (space: Space): React.ReactNode => (
+  <li key={space.id}>{space.attributes.name}</li>
+);
+
 export class SpaceWidget extends React.Component<SpaceWidgetProps> {
   componentDidMount(): void {
     this.props.dispatch(fetchSpaces());
   }
 
   render(): React.ReactNode {
-    const spaceItems: JSX.Element[] = this.props.spaces.map((space: Space): JSX.Element =>
-      <li key={space.id}>{space.attributes.name}</li>
-    );
     return (
       <div className="UI-space-widget">
         <h1>My Spaces</h1>
-        <ul>{spaceItems}</ul>
+        <ul>
+          {this.props.spaces.map(SpaceItem)}
+        </ul>
       </div>
     );
   }
