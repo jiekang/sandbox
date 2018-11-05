35ebcc68cf3db741cf8a9581a31231593862dffa
Jie Kang <jkang@redhat.com>
2018-11-05 19:28:07 +0000

    Update application to run an infinite loop

--- src/main/java/com/redhat/jkang/Main.java
+++ src/main/java/com/redhat/jkang/Main.java
@@ -2,6 +2,8 @@ package com.redhat.jkang;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        while(true) {
+            System.out.println("Hello World!");
+        }
     }
 }
