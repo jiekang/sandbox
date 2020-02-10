4214a07b8fe9e2000fe82496f4b2593ab098200f
Jie Kang <jkang@redhat.com>
2020-02-10 12:20:12 -0500

    Modify Main to create a custom JFR event. Change project to target jdk 11

--- src/main/java/com/redhat/jkang/Main.java
+++ src/main/java/com/redhat/jkang/Main.java
@@ -1,9 +1,20 @@
 package com.redhat.jkang;
 
+import jdk.jfr.Description;
+import jdk.jfr.Event;
+import jdk.jfr.Label;
+
 public class Main {
+
+    @Label("MyEvent")
+    @Description("Basic event")
+    public static class MyEvent extends Event {
+        @Label("Message")
+        String message;
+    }
     public static void main(String[] args) {
-        while(true) {
-            System.out.println("Hello World!");
-        }
+        MyEvent e = new MyEvent();
+        e.message = "Hello World!";
+        e.commit();
     }
 }
