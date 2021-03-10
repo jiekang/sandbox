9809572b297b03b5de5db602dfb0863565b5cea5
Jie Kang <jkang@redhat.com>
2021-03-10 16:16:23 -0500

    Add call to start, stop and dump a recording

--- src/main/java/com/redhat/jkang/Main.java
+++ src/main/java/com/redhat/jkang/Main.java
@@ -1,8 +1,12 @@
 package com.redhat.jkang;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import jdk.jfr.Description;
 import jdk.jfr.Event;
 import jdk.jfr.Label;
+import jdk.jfr.Recording;
 
 public class Main {
 
@@ -12,9 +16,15 @@ public class Main {
         @Label("Message")
         String message;
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        Recording r = new Recording();
+        r.start();
         MyEvent e = new MyEvent();
         e.message = "Hello World!";
         e.commit();
+
+        r.stop();
+        r.dump(Paths.get("recording.jfr"));
     }
 }
